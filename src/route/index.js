e3a3493cbe4224b7e127a8468f45823ca11d2da7
bakulina <bakulinaelena800@gmail.com>
2023-06-23 16:01:11 +0200

    add

--- src/route/index.js
+++ src/route/index.js
@@ -9,4 +9,17 @@ router.get('/', function (req, res) {
   })
 })
 
+router.get('/users', function (req, res) {
+  res.render('user-list', {
+    layout: 'header',
+    title: 'user list page',
+
+    data: {
+      userlist: [{ id: 5123, name: 'Ivan'isAdmin:true },
+                { id: 85677, name: 'Lena'isAdmin:false },
+    ],
+    },
+  })
+})
+
 module.exports = router
